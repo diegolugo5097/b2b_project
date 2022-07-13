ee727be3d0e93eedb5dc9d7ffc9e45bcbc0b1a9e
diegolugo5097 <vitaly818@hotmail.com>
2022-07-13 16:56:20 -0500

    first commit

---
+++ _nuxt/static/1657749246/inspire/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1657749246",layout:"default",error:a,state:{store:{}},serverRendered:true,routePath:"\u002Finspire",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
